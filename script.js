a9dc0be9e74d377b7560cdba1ec4d0c2a4af6695
markymelnik <markemelnik@gmail.com>
2022-10-13 16:01:06 -0400

    Second commit

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Rock, Paper, Scissors
+
+// Random generation of rock, paper, or scissors from the computer side
+
 const max = 4;
 const min = 1;
 const randomNum = Math.floor(Math.random() * (max - min) + min);
 
-function choiceConversion(randomNum) {
+function getComputerChoice(randomNum) {
     switch(randomNum) {
       case 1: 
         return "rock";
@@ -13,12 +17,28 @@ function choiceConversion(randomNum) {
     } 
 }
 
-let computerChoice = choiceConversion(randomNum);
+let computerSelection = getComputerChoice(randomNum);
 
+// All possible outcomes based on user input and computer selection
 
-
-
-
+function playRound(mySelection, computerSelection) {
+  mySelection = mySelection.toLowerCase();
+  if (mySelection == computerSelection) {
+    return "Tie! No winner."
+  } else if (mySelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock beats scissors."
+  } else if (mySelection == "rock" && computerSelection == "paper") {
+    return "You lose! Paper beats rock."
+  } else if (mySelection == "paper" && computerSelection == "rock") {
+    return "You win! Paper beats rock."
+  } else if (mySelection == "paper" && computerSelection == "scissors") {
+    return "You lose! Scissors beats paper."
+  } else if (mySelection == "scissors" && computerSelection == "rock") {
+    return "You lose! Rock beats scissors."
+  } else if (mySelection == "scissors" && computerSelection == "paper") {
+    return "You win! Scissors beats paper."
+  } else return "Invalid input. Try again."
+}
 
 
 
