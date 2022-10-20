5097be7651fafb614c98ab3b6549205359231d32
markymelnik <markemelnik@gmail.com>
2022-10-20 11:49:12 -0400

    Fifth commit; added player prompt, updated playRound and playGame functions

--- script.js
+++ script.js
@@ -1,55 +1,63 @@
-// Rock, Paper, Scissors
+// Rock, Paper, Scissors (Best of 5!)
+
+// Prompt the player for an input and return a valid input of either 'rock', 'paper', or 'scissors'
+
+function getPlayerSelection() {
+  let playerInput = prompt('Rock, paper, or scissors? Best of 5!');
+  return playerInput;
+}
 
 // Random generation of either rock, paper, or scissors from the computer side
 
-function getComputerChoice() {
+function getComputerSelection() {
   const max = 4;
   const min = 1;
   const randomNum = Math.floor(Math.random() * (max - min) + min);   
   switch(randomNum) {
       case 1: 
-        return "rock";
+        return 'rock';
       case 2:
-        return "paper";
+        return 'paper';
       case 3:
-        return "scissors";  
+        return 'scissors';
     } 
 }
 
-// All possible outcomes based on user input and computer selection
+// All possible outcomes based on player selection and computer selection
 
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection) {
-  let computerSelection = getComputerChoice();
-  if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-    console.log("invalid input. try again.")
+function playRound() {
+  let playerSelection = getPlayerSelection();
+  let computerSelection = getComputerSelection();
+  if (playerSelection.toLowerCase() != 'rock' && playerSelection.toLowerCase() != 'paper' && playerSelection.toLowerCase() != 'scissors') {
+    console.log(console.log(`'${playerSelection}' is an invalid input. Choose either rock, paper, or scissors.`));
   } else if (playerSelection == computerSelection) {
-    console.log(`tie. ${playerSelection} is the same as ${computerSelection}. no winner.`);
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("you win! rock beats scissors.")
+    console.log(`Tie. ${playerSelection} is the same as ${computerSelection}. no winner.`);
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    console.log('You won this round! rock beats scissors.')
     playerScore = playerScore + 1;
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("you win! scissors beats paper.") 
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    console.log('You won this round! Scissors beats paper.') 
     playerScore = playerScore + 1;
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("you win! paper beats rock.")
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    console.log('You won this round! Paper beats rock.')
     playerScore = playerScore + 1;
   } else { 
-    console.log(`you lose. ${computerSelection} beats ${playerSelection}.`)
+    console.log(`You lost this round. ${computerSelection} beats ${playerSelection}.`)
     computerScore = computerScore + 1;
   }
 }
 
-// Play five rounds of the game
+// Play five rounds of the game, display score after each round, and display the ultimate result
 
-function playGame(playerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+function playGame() {
   while (playerScore < 3 && computerScore < 3) {
-  playRound(playerSelection);
+  playRound();
+  console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
   }
   if (playerScore == 3) {
-    console.log("You win!\nPlayer Score:", playerScore, "Computer Score:", computerScore)
-  } else console.log("You lose!\nPlayer Score:", playerScore, "Computer Score:", computerScore)
+    console.log(`You won the game!\nPlayer Score: ${playerScore}, Computer Score: ${computerScore}`)
+  } else console.log(`You lost the game! The computer won.\nPlayer Score: ${playerScore}, Computer Score: ${computerScore}`)
 }
